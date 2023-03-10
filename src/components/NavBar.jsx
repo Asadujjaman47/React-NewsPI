9f90778c295ec2e59d0111d78736a5259b749552
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-11 02:35:22 +0600

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,10 @@ export class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav
+          className="navbar navbar-expand-lg bg-body-tertiary bg-dark"
+          data-bs-theme="dark"
+        >
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               NwesPI
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,18 +7,55 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
       import.meta.env.VITE_apiKey
-    }`;
+    }&page=1&pageSize=20`;
     let data = await fetch(url);
     let parseData = await data.json();
     // console.log(data);
-    this.setState({ articles: parseData.articles });
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+    console.log("Prev");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+      import.meta.env.VITE_apiKey
+    }&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page - 1,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("Next");
+
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+        import.meta.env.VITE_apiKey
+      }&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+
+      this.setState({
+        articles: parseData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
 
   render() {
     return (
@@ -31,7 +68,7 @@ export class News extends Component {
                 <NewsItem
                   title={element.title ? element.title : ""}
                   description={
-                    element.description ? element.description.slice(0, 100) : ""
+                    element.description ? element.description.slice(0, 140) : ""
                   }
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
@@ -39,6 +76,23 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
